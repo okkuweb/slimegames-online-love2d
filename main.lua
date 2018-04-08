b3c6ff2143088274a2eba9743d627f3d00800db4
Oskari Saari <okkuweb@gmail.com>
2018-04-08 17:13:34 +0300

    Updated SUIT and added some colliders

--- main.lua
+++ main.lua
@@ -10,18 +10,20 @@ function love.load()
     -- Set pixel graphics filtering
     love.graphics.setDefaultFilter("nearest", "nearest")
     scaling = 6
-    -- Fix and set SUIT colors
-    suit.theme.color = {
-        normal   = {bg = { 0.258823529, 0.258823529, 0.258823529}, fg = {0.737254902,0.737254902,0.737254902}},
-        hovered  = {bg = { 0.196078431,0.6,0.733333333}, fg = {1,1,1}},
-        active   = {bg = {1,0.6,  0}, fg = {1,1,1}}
-    }
+
+    -- GLOBALS
+    -- Scene options
+    screen = {}
+    screen.width = love.graphics.getWidth()
+    screen.height = love.graphics.getHeight()
 
     -- Player 1 options
     p1 = {}
     p1.sprite = love.graphics.newImage("img/slime01.png")
     p1.x = 200
     p1.y = 384
+    p1.right = "d"
+    p1.left = "a"
 
     -- Player 2 options
 
@@ -29,21 +31,64 @@ function love.load()
     -- Ball options
     ball = {}
     ball.size = 2
+    ball.x = 90
+    ball.y = 90
+    ball.speed = 0
     canvas = love.graphics.newCanvas(300, 300)
     love.graphics.setCanvas(canvas)
     love.graphics.circle("fill", ball.size, ball.size, ball.size)
     love.graphics.setCanvas()
+
+    -- Setup colliders
+    collisionText = {}
+    collider = hc.new()
 end
  
 -- Increase the size of the rectangle every frame.
 function love.update(dt)
-    local right = love.keyboard.isDown("d")
-    local left = love.keyboard.isDown("a")
+    -- Handle player controls
+    handleControls(p1)
+    --handleControls(p2)
+
+    -- Handle ball movement
+    ball.speed = ball.speed + 0.05
+    ball.y = ball.y + ball.speed
+    print(ball.speed)
+
+    -- Draw colliders
+    drawColliders()
+    -- Handle ball collider position
+    ballCollision()
+end
+
+function handleControls(p)
+    local right = love.keyboard.isDown(p.right)
+    local left = love.keyboard.isDown(p.left)
     if right then
-        p1.x = p1.x + 5
+        p.x = p.x + 5
     end
     if left then
-        p1.x = p1.x - 5
+        p.x = p.x - 5
+    end
+end
+
+function drawColliders()
+    ballCollider = collider:circle(ball.x+12,ball.y+12,13,100)
+    floorCollider = collider:rectangle(
+        0,screen.height,screen.width,50
+    )
+end
+
+function ballCollision()
+    local floorCollision
+    for shape, delta in pairs(collider:collisions(ballCollider)) do
+        local floor, dx, dy = shape:collidesWith(floorCollider)
+        if floor then
+            floorCollision = 1
+        end
+    end
+    if floorCollision then
+        print("Collided with floor")
     end
 end
  
@@ -52,8 +97,14 @@ function love.draw()
     --Draw player
     love.graphics.setColor(cf.WHITE())
     love.graphics.draw(p1.sprite, p1.x, p1.y, 0, scaling)
+
     -- Draw ball
     love.graphics.setColor(cf.hex "c2c3c7")
-    love.graphics.draw(canvas, 0, 0, 0, scaling)
+    love.graphics.draw(canvas, ball.x, ball.y, 0, scaling)
+
+    -- Draw colliders
+    love.graphics.setColor(cf.RED())
+    ballCollider:draw()
+    floorCollider:draw()
 end
 
